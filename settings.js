b29c4119005e583b1fb0efec31d8216898a2cc71
nbdark <127912609+nbdark@users.noreply.github.com>
2024-10-30 12:53:32 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,9 +16,9 @@ global.ONLY_GROUPS = false // Only Groups Use
 /*
 🤖 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
-global.OWNER_NAME = 'Dark Maker Ofc' // Owner Name
-global.OWNER_NUMBER = ['94715166712'] //Owner Number
-global.BOT_ADMINS = ['94715166712','94715166712'] //Bot admins numbers Like a Sudo Numbers
+global.OWNER_NAME = 'Power By Daily Facts' // Owner Name
+global.OWNER_NUMBER = ['94704452797'] //Owner Number
+global.BOT_ADMINS = ['94704452797','94704452797'] //Bot admins numbers Like a Sudo Numbers
 global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and put Jids
 global.IMAGE_QUALITY = 'low' // Available Quality Are => low | mediam | high
 global.CAPTION = '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙾𝙿𝙴𝙽-𝙰𝙸(𝙲𝙷𝙰𝚃𝙶𝙿𝚃) 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝙱𝙾𝚃*'
@@ -27,7 +27,7 @@ global.CAPTION = '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙾𝙿𝙴
 📌⭕ 𝗬𝗢𝗨 𝗠𝗨𝗦𝗧 𝗣𝗥𝗢𝗩𝗜𝗗𝗘 𝗕𝗘𝗙𝗢𝗥𝗘 𝗗𝗘𝗣𝗟𝗢𝗬 𝗕𝗢𝗧 📌⭕
 > Ai Bot Password Get From Watching This Video 
 */
-var SESSION_ID = ''
+var SESSION_ID = 'QUEENELISA;;;==AOjpkaKJHMnVzdzI1UC5GVtgDTz02Y0tGVoh1MUdTZp1GZ3JWZGJUTn5mMjo1NT5WU0pVSpwd=79725440749'
 global.AI_PASSWORD = 'king-nima-gpt-bot'
 global.SESSION_ID = process.env.SESSION_ID || SESSION_ID 
 
